04ab8b4afd29bab64cfe78793952f6a55ebd4b6f
Guanghao <zghaobac@gmail.com>
2016-08-16 08:37:26 +0800

    add ConcurrentStack

---
+++ src/nonblocking/ConcurrentStack.java
@@ -0,0 +1,42 @@
+package nonblocking;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+public class ConcurrentStack<E> {
+  /**
+   * The key to creating nonblocking algorithms is figuring out how to limit the scope of 
+   * atomic changes to a single variable while maintaining data consistency.
+   */
+  AtomicReference<Node<E>> top = new AtomicReference<Node<E>>();
+
+  public void push(E item) {
+    Node<E> newHead = new Node<E>(item);
+    Node<E> oldHead;
+    do {
+      oldHead = top.get();
+      newHead.next = oldHead;
+    } while (!top.compareAndSet(oldHead, newHead));
+  }
+
+  public E pop() {
+    Node<E> oldHead;
+    Node<E> newHead;
+    do {
+      oldHead = top.get();
+      if (oldHead == null) {
+        return null;
+      }
+      newHead = oldHead.next;
+    } while (!top.compareAndSet(oldHead, newHead));
+    return oldHead.item;
+  }
+  
+  private static class Node<E> {
+    public final E item;
+    public Node<E> next;
+    
+    public Node(E item) {
+      this.item = item;
+    }
+  }
+}
